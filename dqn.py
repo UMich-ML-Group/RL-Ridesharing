e8a442419ef176b3ba68662df9792fbd0dd18a9b
oidelima5031@gmail.com <oidelima5031@gmail.com>
2020-04-08 13:57:54 -0400

    Passengers choose the cars

--- dqn.py
+++ dqn.py
@@ -16,7 +16,7 @@ import copy
 
 
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+                        ('state', 'action', 'reward'))
 
 class ReplayMemory:
 
@@ -57,8 +57,8 @@ class DQN(nn.Module):
 
 
 class DQN_Agent:
-    def __init__(self, env, input_size, output_size, hidden_size, batch_size = 128, lr = 0.001, gamma = .999, eps_start = 0.9, 
-                 eps_end = 0.05, eps_decay = 200, target_update = 300, replay_capacity = 10000, num_episodes = 2000):
+    def __init__(self, env, input_size, output_size, hidden_size, batch_size = 3, lr = 0.001, gamma = .999, eps_start = 0.9, 
+                 eps_end = 0.05, eps_decay = 200, target_update = 1000, replay_capacity = 10000, num_episodes = 10000):
         self.env = env
         self.orig_env = copy.deepcopy(env)
         self.grid_map = env.grid_map
@@ -98,86 +98,32 @@ class DQN_Agent:
         
         self.steps_done = 0
         
-    def get_assignment(self, state, q_values = None, rand = False):
-        # Given a set of q_values for every car and every posible invidual action, use the hungarian method 
-        # to find the best set of actionsor or choose random actions given the constraints of the enviroment 
-        
-        action = np.zeros(self.num_cars)
-
-        cars_status = list(state[0][2:3*self.num_cars:3])
-        pass_status = list(state[0][3*self.num_cars+4::5])
-
-        pass_to_remove = [i for i,passenger in enumerate(pass_status) if passenger != 1]
-        pass_to_match = [i for i in range(self.num_passengers) if i not in pass_to_remove]
-
-          
-        if rand:
-            cars_to_remove = [i for i,car in enumerate(cars_status) if car != 1] 
-            cars_to_match = [i for i in range(self.num_cars) if i not in cars_to_remove]
-            
-            assignments = random.sample(range(len(pass_to_match)+1),len(cars_to_match))
-            
-            for i, assig in enumerate(assignments):
-                if assig == len(pass_to_match):
-                    action[cars_to_match[i]] = self.num_passengers
-                else:
-                    action[cars_to_match[i]] = pass_to_match[assig]
-        else:
-            cars_to_remove = [i for i,car in enumerate(cars_status) if (car != 1 or np.amax(q_values[i]) == q_values[i,-1])]  
-            cars_to_match = [i for i in range(self.num_cars) if i not in cars_to_remove]
-            
-            q_filt = q_values[:, :-1]
-            q_filt= np.delete(q_filt, cars_to_remove, 0)
-            q_filt = np.delete(q_filt, pass_to_remove, 1)
-            
-            row_idx, col_idx = scipy.optimize.linear_sum_assignment(-q_filt)
-            
-            
-            for i, row in enumerate(row_idx):
-                action[cars_to_match[row]] = pass_to_match[col_idx[i]]
-                
-            for i in range(len(cars_to_match)):
-                if i not in row_idx:
-                    action[cars_to_match[i]] = self.num_passengers
-                    
-        action[cars_to_remove] = self.num_passengers #do nothing
-        return action.astype(int)
-        
-        
+    
 
     def select_action(self,state):
         #Select action with epsilon greedy
-              
         sample = random.random()
         
         eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
             math.exp(-1. * self.steps_done / self.eps_decay)
         self.steps_done += 1
-        state = torch.tensor(state, device = self.device, dtype=torch.float).unsqueeze(0)
-        # ERASE
-        # with torch.no_grad():
-        #     self.policy_net.eval() 
-        #     state_high = [1., 2., 0., 1., 4., 0., 2., 0.]
-        #     state_high = torch.tensor(state_high, device = self.device, dtype=torch.float).unsqueeze(0)
-        #     q_values_high = self.policy_net(state_high).cpu().numpy().reshape(self.num_cars, self.num_passengers + 1) #ERASE
-        #     print("Q_values_high: ",q_values_high) #ERASE
-        
+
         if sample > eps_threshold:
             # Choose best action
             with torch.no_grad():
                 
-                self.policy_net.eval() # not sure if right
-                q_values = self.policy_net(state).cpu().numpy().reshape(self.num_cars, self.num_passengers + 1) 
+                self.policy_net.eval() 
+                return self.policy_net(state).view(self.num_passengers, self.num_cars).max(1)[1].view(1,self.num_passengers)
 
-                return self.get_assignment(state, q_values)
         else:
             #Choose random action
-            return self.get_assignment(state, rand = True)
+            return torch.tensor([[random.randrange(self.num_cars) for car in range(self.num_passengers)]], device=self.device, dtype=torch.long)
+
             
 
-
     def random_action(self, state):
-        return self.get_assignment(state, rand = True)
+        return torch.tensor([[random.randrange(self.num_cars) for car in range(self.num_passengers)]], device=self.device, dtype=torch.long)
+    
     
     def get_state(self):
         # Cars (px, py, 1=matched), Passengers(pickup_x, pickup_y, dest_x, dest_y, 1=matched)
@@ -186,74 +132,42 @@ class DQN_Agent:
         passengers = self.passengers
 
         # Encode information about cars
-        cars_vec = np.zeros(3*len(cars))
+        cars_vec = np.zeros(2*len(cars))
         
         for i, car in enumerate(cars):    
-            cars_vec[3*i: 3*i + 3]  = [car.position[0], car.position[1], 1 if car.status == "idle"  else 0]
+            cars_vec[2*i: 2*i + 2]  = [car.position[0], car.position[1]]
 
         # Encode information about passengers
-        passengers_vec = np.zeros(5*len(passengers))
+        passengers_vec = np.zeros(4*len(passengers))
         for i, passenger in enumerate(passengers):
-            passengers_vec[5*i: 5*i + 5]  = [passenger.pick_up_point[0], passenger.pick_up_point[1],
-                                             passenger.drop_off_point[0],passenger.drop_off_point[1],  
-                                             1 if passenger.status == 'wait_pair' else 0]
+            passengers_vec[4*i: 4*i + 4]  = [passenger.pick_up_point[0], passenger.pick_up_point[1],
+                                             passenger.drop_off_point[0],passenger.drop_off_point[1]]
 
-        return np.concatenate((cars_vec, passengers_vec))
+        return torch.tensor(np.concatenate((cars_vec, passengers_vec)), device= self.device, dtype=torch.float).unsqueeze(0)
     
     
     def train(self):
         
-        
         for episode in range(self.num_episodes):
+            
             #self.reset() 
             self.reset_orig_env()
-            state = self.get_state()
-
-            for t in count():
-                
-                    
-                action = self.select_action(state)
-                #action = self.random_action([state])
-                reward, done = self.env.dqn_step(action)
-                # if (state[2] == 1):
-                #     print("Action: ", action)
-                #     print("Step: ", t)
-                #     input("Press enter to step")
-                
-                    
-                    
-                #print("Action: ", action)
-                #print("Reward: ", reward)
-                #print("Done: ", done)
-                # #print(self.grid_map)
-                # print("Step: ", t)
-                #print("State :", state)
-                #input("Press enter to step")
-                #self.grid_map.visualize()
-                
-
-                next_state = self.get_state()
-                    
-                 
-                               
-                self.memory.push(torch.tensor(state, device = self.device, dtype=torch.float).unsqueeze(0), 
-                                 torch.tensor(action, device = self.device, dtype=torch.long).unsqueeze(0), 
-                                 torch.tensor(next_state, device = self.device, dtype=torch.float).unsqueeze(0) if not done else None, 
-                                 torch.tensor(reward, device = self.device, dtype=torch.float).unsqueeze(0) )  
-                
-                # Move to the next state 
-                
-                state = next_state  
-                
-                # Perform one step of the optimization (on the target network)
-                self.optimize_model()
-                
-                if done:
-                    self.episode_durations.append(t + 1)
-                    self.plot_durations()
-                    self.plot_loss_history()
-                    break         
             
+            state = self.get_state()  
+            
+            action = self.select_action(state)
+            ction = self.random_action([state])
+            
+            reward, duration = self.env.dqn_step(action)
+            
+            self.memory.push(state, action, torch.tensor(reward, device = self.device, dtype=torch.float).unsqueeze(0))  
+            
+            self.optimize_model()
+            
+            self.episode_durations.append(duration)
+            self.plot_durations()
+            self.plot_loss_history()
+                    
             # Update the target network, copying all weights and biases in DQN
             print("Episode: ", episode)
             if episode % self.target_update == 0:
@@ -277,81 +191,26 @@ class DQN_Agent:
         self.grid_map.init_zero_map_cost()
 
         
-        
-           
 
     def optimize_model(self):
         if len(self.memory) < self.batch_size:
             return
         transitions = self.memory.sample(self.batch_size)
-        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # detailed explanation). This converts batch-array of Transitions
-        # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
     
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=self.device, dtype=torch.bool)
-        non_final_next_states = torch.cat([s for s in batch.next_state
-                                                    if s is not None])
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
-
-       
         
-        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # columns of actions taken. These are the actions which would've been taken
-        # for each batch state according to policy_net
-
-        #self.policy_net.train()
-        #self.target_net.train()
-        #state_action_values = self.policy_net(state_batch).view(self.batch_size, self.num_cars, self.num_passengers + 1).gather(2, action_batch.unsqueeze(2)).squeeze()
-        state_action_values = self.policy_net(state_batch).gather(1,action_batch)
-
-        #state_action_values = state_action_values# .unsqueeze(1)
-        #print(self.policy_net(state_batch).view(self.batch_size, self.num_cars, self.num_passengers + 1))
-        #print(action_batch.unsqueeze(2))
-        #print(state_action_values)
-        
-        
-        # Compute V(s_{t+1}) for all next states.
-        # Expected values of actions for non_final_next_states are computed based
-        # on the "older" target_net; selecting their best reward with max(1)[0].
-        # This is merged based on the mask, such that we'll have either the expected
-        # state value or 0 in case the state was final.
-        #self.target_net.train()
-        
-        #next_state_action_values = torch.zeros((self.batch_size, self.num_cars), device=self.device)
-        next_state_action_values = torch.zeros((self.batch_size, self.num_cars), device=self.device)
-        next_q_values = self.target_net(non_final_next_states).view(non_final_next_states.size()[0], self.num_cars, self.num_passengers + 1) 
-        #print(self.target_net(non_final_next_states).size())
-        #print(self.target_net(non_final_next_states).max(1)[0].detach().size())
-        #print(self.target_net(non_final_next_states).max(1)[0])
-        
-        non_final_mask = non_final_mask.unsqueeze(1)
-
-        
-        #next_state_action_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach() # erase
-
-
-        
-        next_action_batch = self.get_best_batch_actions(non_final_next_states, next_q_values).squeeze()
-        #next_action_batch = next_action_batch.unsqueeze(1) # CHANGE LATER
-        # non_final_mask = non_final_mask.unsqueeze(1) # CHANGE LATER
-
-        next_state_action_values[non_final_mask] = next_q_values.gather(2, next_action_batch.view((-1,1,1))).squeeze().detach()
+        self.policy_net.train()
+        state_action_values = self.policy_net(state_batch).view(self.batch_size, self.num_passengers, self.num_cars).gather(2,action_batch.unsqueeze(2)).squeeze()
 
         # Compute the expected Q values
-        expected_state_action_values = (next_state_action_values * self.gamma) + reward_batch
+        expected_state_action_values = reward_batch
+        
 
         # Compute Huber loss
-        #loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
-        #print(state_batch)
-        #print(reward_batch)
-        
 
         self.loss_history.append(loss.item())
 
@@ -362,17 +221,8 @@ class DQN_Agent:
         for param in self.policy_net.parameters():
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
-        #for name, param in self.policy_net.named_parameters():
-        #    if param.requires_grad:
-        #        print(name, param.data)
-        #print(loss)
 
-    def get_best_batch_actions(self, states, q_values):
-        actions = np.zeros((q_values.size()[0], self.num_cars))
-        for i,(state,q) in enumerate(zip(states,q_values)):
-            q_value = q.detach().cpu().numpy().reshape(self.num_cars, self.num_passengers + 1) 
-            actions[i] = self.get_assignment(state.unsqueeze(0), q_value)      
-        return torch.tensor(actions, device = self.device, dtype=torch.long)
+
 
     def plot_durations(self):
         print("Saving durations plot ...")
@@ -397,10 +247,10 @@ class DQN_Agent:
         plt.savefig("Loss_history")
 
 if __name__ == '__main__':
-    num_cars = 1
-    num_passengers = 3
+    num_cars = 3
+    num_passengers = 4
     
-    grid_map = GridMap(1, (5,5), num_cars, num_passengers)
+    grid_map = GridMap(1, (7,7), num_cars, num_passengers)
     cars = grid_map.cars
     passengers = grid_map.passengers
     env = Environment(grid_map)
@@ -409,8 +259,8 @@ if __name__ == '__main__':
     #path = m.plan_path((0,0),(5,5))
     #sprint(path)
     #m.visualize()
-    input_size = 3*num_cars + 5*num_passengers # cars (px, py, 1=matched), passengers(pickup_x, pickup_y, dest_x, dest_y, 1=matched)
-    output_size = num_cars * (num_passengers + 1) # num_cars * (num_passengers + 1)
+    input_size = 2*num_cars + 4*num_passengers # cars (px, py), passengers(pickup_x, pickup_y, dest_x, dest_y)
+    output_size = num_cars * num_passengers  # num_cars * (num_passengers + 1)
     hidden_size = 100
     agent = DQN_Agent(env, input_size, output_size, hidden_size)
     agent.train()
--- environment.py
+++ environment.py
@@ -53,56 +53,70 @@ class Environment:
     def dqn_step(self, action):
         
         grid_map = self.grid_map
- 
         cars = grid_map.cars
         passengers = grid_map.passengers
-        reward = [0]*len(cars)
+        reward = [0]*len(passengers)
+        done = False
         
-        for i, act in enumerate(action):
-            if act < len(passengers) and passengers[act].status == 'wait_pair':
-                car = cars[i]
-                passenger = passengers[act]
-                car.pair_passenger(passenger)
-                pick_up_path = grid_map.plan_path(car.position, passenger.pick_up_point)
-                drop_off_path = grid_map.plan_path(passenger.pick_up_point, passenger.drop_off_point)
-                car.assign_path(pick_up_path, drop_off_path)
-                
-        for passenger in passengers:
-            if passenger.status == 'wait_pair' or passenger.status == 'wait_pick':
-                passenger.waiting_steps += 1
+        duration = 0
+        
+        while not done:
+            
+            #print("Action: ", action)
+            #print(self.grid_map)
+            #input("Press enter to step")
+            #self.grid_map.visualize()
+            
+        
+            for i, act in enumerate(action[0]):
+                if cars[act].status == "idle" and passengers[i].status == "wait_pair":
+                    car = cars[act]
+                    passenger = passengers[i]
+                    car.pair_passenger(passenger)
+                    pick_up_path = grid_map.plan_path(car.position, passenger.pick_up_point)
+                    drop_off_path = grid_map.plan_path(passenger.pick_up_point, passenger.drop_off_point)
+                    car.assign_path(pick_up_path, drop_off_path)
+                    
+            
 
-        # move car
 
-        for i,car in enumerate(cars):
+            
+            for i,car in enumerate(cars):
 
-            if car.status == 'idle':
-                #reward[i] -= 1 uncomment
-                continue
+                if car.status == 'idle':
+                    continue
 
-            # init require step
-            if car.required_steps is None:  # init
-                car.required_steps = self.grid_map.map_cost[(car.position, car.path[0])]
+                # init require step
+                if car.required_steps is None:  # init
+                    car.required_steps = self.grid_map.map_cost[(car.position, car.path[0])]
 
-            # pick up or drop off will take one step
-            if car.status == 'picking_up' and car.position == car.passenger.pick_up_point: # picking up
-                reward[i] += 10
-                car.pick_passenger()
-            elif car.status == 'dropping_off' and car.position == car.passenger.drop_off_point:  # dropping off
-                car.drop_passenger()
-            else:
-                # try to move
-                if car.passenger.status == "wait_pick":
-                    reward[i] -= 1 #uncomment
-                    pass
-                if car.required_steps > 0:  # decrease steps
-                    car.required_steps -= 1
-                elif car.required_steps == 0: # move
-                    car.move()
-                    if car.path:
-                        car.required_steps = self.grid_map.map_cost[(car.position, car.path[0])]
-
-        done = False not in [passenger.status == "dropped" for passenger in passengers]             
-        return reward, done
+                # pick up or drop off will take one step
+                if car.status == 'picking_up' and car.position == car.passenger.pick_up_point: # picking up
+                    car.pick_passenger()
+                elif car.status == 'dropping_off' and car.position == car.passenger.drop_off_point:  # dropping off
+                    car.drop_passenger()
+                else:
+                    # try to move
+                    if car.required_steps > 0:  # decrease steps
+                        car.required_steps -= 1
+                    elif car.required_steps == 0: # move
+                        car.move()
+                        if car.path:
+                            car.required_steps = self.grid_map.map_cost[(car.position, car.path[0])]
+                            
+            for passenger in passengers:
+                if passenger.status == 'wait_pair' or passenger.status == 'wait_pick':
+                    passenger.waiting_steps += 1
+            
+            done = False not in [passenger.status == "dropped" for passenger in passengers] 
+            
+            duration += 1
+        
+        reward = [-passenger.waiting_steps for passenger in passengers]
+                    
+        return reward, duration
+    
+    
             
 
                     
